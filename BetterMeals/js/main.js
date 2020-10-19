6b1710006aad7c4b066b5d94fe2c5042742fb1fa
Kellz <kla172@sfu.ca>
2020-10-18 20:54:33 -0700

    Added a new navigation template with java script

--- BetterMeals/js/main.js
+++ BetterMeals/js/main.js
@@ -1,3 +1,8 @@
-$('document').ready(function(){
-	document.write(5 + 6);
- });
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
