639248ab0009e8969722def17d0455d3d6458ded
Kellz <kla172@sfu.ca>
2020-10-22 00:48:44 -0700

    Added Modals to Product Page and JS
    uncommented the modal js

--- BetterMeals/js/main.js
+++ BetterMeals/js/main.js
@@ -1,26 +1,26 @@
 window.onload = function(){
 
-	// // Get the modal
-	// var modal = document.getElementById("myModal");
+	// Get the modal
+	var modal = document.getElementById("myModal");
 
-	// // modal2
-	// var modal2 = document.getElementById("myModal2");
+	// modal2
+	var modal2 = document.getElementById("myModal2");
 
-	// // Get the button that opens the modal
-	// var btn = document.getElementById("myBtn");
+	// Get the button that opens the modal
+	var btn = document.getElementById("myBtn");
 
-	// // Get the image and insert it inside the modal - use its "alt" text as a caption
-	// var img = document.getElementById("myImg");
+	// Get the image and insert it inside the modal - use its "alt" text as a caption
+	var img = document.getElementById("myImg");
 
-	// var add2cart = document.getElementById("myAdd2cart");
+	var add2cart = document.getElementById("myAdd2cart");
 
-	// // Get the <span> element that closes the modal
-	// var span = document.getElementsByClassName("close")[0];
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
 
-	// // Get the button that opens the modal
-	// var closebtn = document.getElementById("closeBtn");
+	// Get the button that opens the modal
+	var closebtn = document.getElementById("closeBtn");
 
-	// var header = document.getElementById("menuNav");
+	var header = document.getElementById("menuNav");
 
 	var sticky = 5;
 
@@ -35,36 +35,36 @@ window.onload = function(){
 
 
 
-	// // When the user clicks the button, open the modal
-	// btn.onclick = function() {
-	//   modal.style.display = "block";
-	// }
+	// When the user clicks the button, open the modal
+	btn.onclick = function() {
+	  modal.style.display = "block";
+	}
 
-	// add2cart.onclick = function() {
-	//   modal2.style.display = "block";
-	// }
+	add2cart.onclick = function() {
+	  modal2.style.display = "block";
+	}
 
-	// // When the user clicks on <span> (x), close the modal
-	// span.onclick = function() {
-	//   modal.style.display = "none";
-	// }
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	  modal.style.display = "none";
+	}
 
-	// closebtn.onclick = function() {
-	//   modal2.style.display = "none";
-	// }
+	closebtn.onclick = function() {
+	  modal2.style.display = "none";
+	}
 
 
 
-	// img.onclick = function(){
-	// 	modal.style.display = "block";
-	// }
+	img.onclick = function(){
+		modal.style.display = "block";
+	}
 
-	// // When the user clicks anywhere outside of the modal, close it
-	// window.onclick = function(event) {
-	//   if (event.target == modal) {
-	//     modal.style.display = "none";
-	//   }
-	// }
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	  if (event.target == modal) {
+	    modal.style.display = "none";
+	  }
+	}
 
 	window.onscroll = function(){makeSticky()};
 
@@ -92,6 +92,3 @@ function makeSticky() {
     console.log('goodbye');
   }
 }
-
-
-
