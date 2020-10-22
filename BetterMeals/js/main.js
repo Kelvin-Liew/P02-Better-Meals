f0918418b670c2510f1e9faff8c3a21a01c50573
chamira <chamiraperera97@gmail.com>
2020-10-21 20:30:01 -0700

    about page made and navigation bar made

--- BetterMeals/js/main.js
+++ BetterMeals/js/main.js
@@ -64,3 +64,21 @@ function myFunction() {
 		x.className = "topnav";
 	}
 }
+
+//chamira added
+let mainNav=document.getElementById('main-nav');
+let navbarToggle=document.getElementById('navbar-toggle');
+
+navbarToggle.addEventListener('click',function(){
+
+    if(this.classList.contains('active')){
+        mainNav.style.display="none";
+        this.classList.remove('active');
+    }
+    else{
+        mainNav.style.display="flex";
+        this.classList.add('active');
+
+    }
+});
+//chamira added end
